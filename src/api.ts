2178401fa8c1982d3cc83932dd57a5272ecab08b
hossein <mdoroud97@gmail.com>
2023-05-13 22:45:18 +0330

    feat: add logger + hanlde errors

--- src/api.ts
+++ src/api.ts
@@ -3,10 +3,12 @@ import { PORT, DBURL, CORS_ORIGINS } from "./config";
 import cors from "cors";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import { router as favoriteRouter } from "./routes/favorite.router";
-import { router as profileRouter } from "./routes/profile.router";
-import { router as simulatorRouter } from "./routes/simulator.router";
+import expressWinston from "express-winston";
+import winston from "winston";
+import { router } from "./routes";
+import statusCode from "./constants/statusCode";
 
+// db connection
 mongoose
   .connect(`${DBURL}`, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
@@ -14,13 +16,54 @@ mongoose
   });
 
 const app = express();
+
+// handle CORS
 app.use(cors({ origin: CORS_ORIGINS }));
+
+// parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(favoriteRouter);
-app.use(profileRouter);
-app.use(simulatorRouter);
+
+expressWinston.requestWhitelist.push("body");
+
+app.use(
+  expressWinston.logger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    ),
+    expressFormat: true,
+  })
+);
+
+// import routes
+app.use("/api", router);
+
+app.use("*", (req, res, next) => {
+  next({
+    error: `${req.originalUrl} not found`,
+    status: statusCode.HTTP_NOT_FOUND,
+  });
+});
+
+// error handling
+app.use((err, req, res, next) => {
+  return res
+    .status(err.status || statusCode.HTTP_INTERNAL_SERVER_ERROR)
+    .json({ error: err.error || err });
+});
+
+app.use(
+  expressWinston.errorLogger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    ),
+  })
+);
 
 app.listen(PORT, () =>
-  console.log(`✅  Ready on port http://localhost:${PORT}`)
+  console.log(`✅ Ready on port http://localhost:${PORT}`)
 );
